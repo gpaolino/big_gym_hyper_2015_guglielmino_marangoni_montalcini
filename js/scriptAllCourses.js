b0b1aab0494dbf4c0f898a0860028edbf1fc63e2
unknown <gp.guglielminopaolo@me.com>
2015-05-29 14:16:19 +0200

    Bottoni e immagini leggermente arrotondati

--- js/scriptAllCourses.js
+++ js/scriptAllCourses.js
@@ -25,7 +25,7 @@ function ready(){
                 }
                 if(oldCourseCat != courses[i].course_cat) {
                     oldCourseCat = courses[i].course_cat;
-                    el+="<div class='col-xs-6 col-md-3' style='margin-bottom:2%'><h4>"+courses[i].ccfn+":</h4><img class='media-object' src='"+courses[i].image+"' alt='Generic placeholder image'></div>";    
+                    el+="<div class='col-xs-6 col-md-3' style='margin-bottom:2%'><h4>"+courses[i].ccfn+":</h4><img class='media-object rounded-img' src='"+courses[i].image+"' alt='Generic placeholder image'></div>";    
 
                     column++;
                     if(courses[i].active == 1) {
