02f128fb46030b3046a63b7673baf1ed724ace9f
Daniele Marangoni <DanieleMarangoni@MBP-di-Daniele.lan>
2015-05-31 13:16:49 +0200

    modicato all courses: ora appaiono in ordine alfabetico verticale

--- js/scriptAllCourses.js
+++ js/scriptAllCourses.js
@@ -15,49 +15,134 @@ function ready(){
             var courses=JSON.parse(response);
             var column = 0; 
             var oldCourseCat = -1; 
-            var el="";
-            var el2="";
-            var contenuto=$("contenuto");
-
+            var courses2=courses;  
+            var el = ""; 
+            var el2 = "";
+            var insertedCC = 0; 
+            var numberOfCC = 0; 
+            var countedCC = 0; 
+            var offsetCC = 0;
+            var offsetCourses = -1; 
+            var lasti = -1; 
+            var lastj = -1;
             for(var i=0; i<courses.length;i++){
-                if(column%4==0){
-                    //Apro riga e stampo elemento con indice pari
-                    el+='<div class="row" style="margin-bottom: 35px">';
-                }
                 if(oldCourseCat != courses[i].course_cat) {
                     oldCourseCat = courses[i].course_cat;
-                    el+="<div class='col-sm-3'><h4>"+courses[i].ccfn+":</h4><img class='media-object rounded-img' src='"+courses[i].image+"' alt='Generic placeholder image'></div>";    
+                    numberOfCC++; 
+                }
+            }
+            if(numberOfCC%2 == 0) {
+                offsetCC = numberOfCC/2;
+            } else {
+                offsetCC = Math.floor(numberOfCC/2) +1; 
+            }
+            oldCourseCat = -1; 
+            for(var i=0; i<courses.length;i++){
+                
+                offsetCourses++;
+                if(oldCourseCat != courses[i].course_cat) {
+                    oldCourseCat = courses[i].course_cat;
 
-                    column++;
-                    if(courses[i].active == 1) {
-                        el+="<div class='col-sm-3' style='margin-top: 35px'><li><a href='course.html?par="+courses[i].cfn+"&par2=allCourses'>"+courses[i].cfn+"<span class='sr-only'>(current)</span></a></li>";
-                    } else {
-                        el+="<div class='col-sm-3' style='margin-top: 35px'><li><a class='inactiveLink' href='#'>"+courses[i].cfn+"<span class='sr-only'>(current)</span></a></li>";
-
+                    countedCC++; 
+                    if(countedCC > offsetCC) {
+                        offsetCourses --; 
+                        break; 
                     }
                 }
-                while(i < courses.length -1 && courses[i+1].course_cat == oldCourseCat) {
-                    i++;
-                    if(courses[i].active == 1) {
-                        el+="<li><a href='course.html?par="+courses[i].cfn+"&par2=allCourses'>"+courses[i].cfn+"<span class='sr-only'>(current)</span></a></li>";
-                    } else {
-                        el+="<li><a class='inactiveLink' href='#'>"+courses[i].cfn+"<span class='sr-only'>(current)</span></a></li>";
-                    }
-                }
-                el+="</div>";
-                column++; 
-                if(column%4==0){
-                    el+='</div><!--/row-->';
-                }             //Se l'ultimo elemento chiudo la riga
-                else if(i==courses.length-1){
-                    el+='</div><!--/row-->';
-                }
-
             }
 
+            lastj=offsetCourses;
+            var first =0; 
+
+            while(insertedCC < numberOfCC) {
+                if(first == 0) {
+
+                    for(var i=lasti+1; i<courses.length;i++){
+                        if(column%4==0){
+                            //Apro riga e stampo elemento con indice pari
+                            el+='<div class="row" style="margin-bottom: 35px">';
+                        }
+                        if(oldCourseCat != courses[i].course_cat) {
+
+                            oldCourseCat = courses[i].course_cat;
+                            el+="<div class='col-sm-3'><h4>"+courses[i].ccfn+":</h4><img class='media-object rounded-img' src='"+courses[i].image+"' alt='Generic placeholder image'></div>";    
+
+                            column++;
+                            if(courses[i].active == 1) {
+                                el+="<div class='col-sm-3' style='margin-top: 35px'><li><a href='course.html?par="+courses[i].cfn+"&par2=allCourses'>"+courses[i].cfn+"<span class='sr-only'>(current)</span></a></li>";
+                            } else {
+                                el+="<div class='col-sm-3' style='margin-top: 35px'><li><a class='inactiveLink' href='#'>"+courses[i].cfn+"<span class='sr-only'>(current)</span></a></li>";
+
+                            }
+                        }
+                        while(i < courses.length -1 && courses[i+1].course_cat == oldCourseCat) {
+                            i++;
+                            if(courses[i].active == 1) {
+                                el+="<li><a href='course.html?par="+courses[i].cfn+"&par2=allCourses'>"+courses[i].cfn+"<span class='sr-only'>(current)</span></a></li>";
+                            } else {
+                                el+="<li><a class='inactiveLink' href='#'>"+courses[i].cfn+"<span class='sr-only'>(current)</span></a></li>";
+                            }
+                        }
+                        el+="</div>";
+                        column++; 
+                        if(column%4==0){
+                            el+='</div><!--/row-->';
+                        }             //Se l'ultimo elemento chiudo la riga
+                        else if(i==courses.length-1){
+                            el+='</div><!--/row-->';
+                        }
+                        insertedCC++;
+
+                        lasti = i;
+                        first=1; 
+                        break; 
+                    }
+                } else {
+
+                    for(var j=lastj+1; j<courses.length;j++){
+                        if(column%4==0){
+                            //Apro riga e stampo elemento con indice pari
+                            el+='<div class="row" style="margin-bottom: 35px">';
+                        }
+                        if(oldCourseCat != courses[j].course_cat) {
+                            oldCourseCat = courses[j].course_cat;
+                            el+="<div class='col-sm-3'><h4>"+courses[j].ccfn+":</h4><img class='media-object rounded-img' src='"+courses[j].image+"' alt='Generic placeholder image'></div>";    
+
+                            column++;
+                            if(courses[j].active == 1) {
+                                el+="<div class='col-sm-3' style='margin-top: 35px'><li><a href='course.html?par="+courses[j].cfn+"&par2=allCourses'>"+courses[j].cfn+"<span class='sr-only'>(current)</span></a></li>";
+                            } else {
+                                el+="<div class='col-sm-3' style='margin-top: 35px'><li><a class='inactiveLink' href='#'>"+courses[j].cfn+"<span class='sr-only'>(current)</span></a></li>";
+
+                            }
+                        }
+                        while(j < courses.length -1 && courses[j+1].course_cat == oldCourseCat) {
+                            j++;
+                            if(courses[j].active == 1) {
+                                el+="<li><a href='course.html?par="+courses[j].cfn+"&par2=allCourses'>"+courses[j].cfn+"<span class='sr-only'>(current)</span></a></li>";
+                            } else {
+                                el+="<li><a class='inactiveLink' href='#'>"+courses[j].cfn+"<span class='sr-only'>(current)</span></a></li>";
+                            }
+                        }
+                        el+="</div>";
+                        column++; 
+                        if(column%4==0){
+                            el+='</div><!--/row-->';
+                        }             //Se l'ultimo elemento chiudo la riga
+                        else if(j==courses.length-1){
+                            el+='</div><!--/row-->';
+                        }
+                        insertedCC++;
+
+                        lastj=j
+                        first=0; 
+                        break; 
+                    }
+                }
+            }
             $("contenuto").html(el); 
             $("contenuto").append('</div><!--/row-->');   
-            
+
             el2+="<ul class='nav nav-tabs'>";
             el2+="<li role='presentation' class='active'><a href='allCourses.html'>All Courses</a></li>";
             el2+="<li role='presentation'><a href='coursesByLevel.html'>Courses by Level</a></li>";
