aa71a840abf9504b3c5baefe74ef2be32a4e3679
unknown <gp.guglielminopaolo@me.com>
2015-06-04 01:10:31 +0200

    Miglioramento dello stile di alcune pagine

--- js/scriptCoursesByLevel.js
+++ js/scriptCoursesByLevel.js
@@ -18,7 +18,7 @@ function ready(){
             var el2="";
             var lev_vector = ["Beginner", "Intermediate", "Advanced"];
 
-            el+='<div class="row row-offcanvas row-offcanvas-right"><div class="col-xs-12 col-sm-9">';
+            //el+='<div class="row row-offcanvas row-offcanvas-right"><div class="col-xs-12 col-sm-9">';
 
             for(var i=0; i<courses.length;i++){
                 if(courses[i].level != level) {
@@ -27,10 +27,10 @@ function ready(){
                         el+="</div>";
                         el+='</div>';
                     }
-                    el+='<div class="row" style="margin-bottom: 35px">';
+                    el+='<div class="col-sm-10"><h1>Select your favorite course:</h1></div><div class="row" style="margin-bottom: 35px">';
 
-                    el+="<div class='col-sm-4'><h4>"+lev_vector[level]+"</h4><img class='img-column2 rounded-img' src='img/courses/level"+courses[i].level+".png' alt='Generic placeholder image'></div>";
-                    el+="<div class='col-sm-8' style='margin-top: 35px'>"; 
+                    el+="<div class='col-sm-3'><h4>"+lev_vector[level]+"</h4><img class='img-column2 rounded-img' src='img/courses/level"+courses[i].level+".png' alt='Generic placeholder image'></div>";
+                    el+="<div class='col-sm-9' style='margin-top: 35px'>"; 
                     if(courses[i].active == 1) {
                         el+="<li><a href='course.html?par="+courses[i].full_name+"&par2=coursesByLevel'>"+courses[i].full_name+"<span class='sr-only'>(current)</span></a></li>";
                     } else {
@@ -45,7 +45,7 @@ function ready(){
                 }
             }
             $("contenuto").html(el); 
-            $("contenuto").append('</div><!--/.col-xs-12.col-sm-9--></div><!--/row-->');     
+            $("contenuto").append('</div><!--/row-->');     
 
             el2+="<ul class='nav nav-tabs'>";
             el2+="<li role='presentation'><a href='allCourses.html'>All Courses</a></li>";
